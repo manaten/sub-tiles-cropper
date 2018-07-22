6a51af49d9c84e1f8463d73198e1ff22185e989c
manaten <manaten@manaten.net>
2018-07-23 01:57:16 +0900

    feat(subtile-cropper): initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# pixel-croper
+# sub-tiles-cropper
+An utility for crop sub-tiles from images.
+
+---
+
+# usage
+
+```
+node index.js input.png -s setting.json
+```
+
+## setting.json
+
+```json
+{
+  "borderColor": 4278255615, // hex color code for border-color(RGBA)
+  "names": [
+    "subtile1",
+    "subtile2"
+  ] // names for sutile files
+}
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+const Jimp = require('jimp');
+const path = require('path');
+const argv = require('yargs-parser')(process.argv.slice(2));
+
+const setting = argv.s ? require(path.resolve(argv.s)) : { borderColor: 0xFF00FFFF, names: [] };
+const BORDER_COLOR = setting.borderColor;
+
+function getSubTilePixel(image, startX, startY) {
+  const { width, height } = image.bitmap;
+
+  for (let x2 = startX; x2 < width;  x2++) {
+    if (
+      image.getPixelColor(x2, startY) !== BORDER_COLOR
+      && image.getPixelColor(x2 + 1, startY) === BORDER_COLOR
+    ) {
+      
+      for (let y2 = startY; y2 < height; y2++) {
+        if (
+          image.getPixelColor(startX, y2) !== BORDER_COLOR
+          && image.getPixelColor(startX, y2 + 1) === BORDER_COLOR
+        ) {
+          return [ startX , startY, x2 - startX, y2 - startY];
+        }
+      }
+    }
+  }
+  return null;
+}
+
+async function main() {
+  const image = await Jimp.read(path.resolve(argv._[0]));
+
+  const { width, height } = image.bitmap;
+  console.log(`width:${width} height:${height}`);
+
+  const subTiles = [];
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width;  x++) {
+      if (
+        image.getPixelColor(x, y) === BORDER_COLOR
+        && image.getPixelColor(x + 1, y) === BORDER_COLOR
+        && image.getPixelColor(x, y + 1) === BORDER_COLOR
+        && image.getPixelColor(x + 1, y + 1) !== BORDER_COLOR
+      ) {
+        const subTile = getSubTilePixel(image, x + 1, y + 1);
+        if (subTile) {
+          subTiles.push(subTile);
+        }
+      }
+    }
+  }
+
+  const basePath = path.resolve(argv._[0], '../');
+  for (let i = 0; i < subTiles.length; i++) {
+    await image
+      .clone()
+      .crop(...subTiles[i])
+      .write(path.resolve(basePath, `${setting.names[i] || i}.${image.getExtension()}`))
+  }
+}
+
+main().catch(e => console.error(e));
